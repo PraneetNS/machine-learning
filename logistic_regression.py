9d819bb7d35e21ac538acaca7cc91d970ccb2ec4
PraneetNS <savantpraneet@gmail.com>
2026-03-10 23:41:47 +0530

    Logistic Regression

---
+++ logistic_regression.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+X = np.array([0.2,0.4,0.6,0.8])
+y = np.array([0,0,1,1])
+
+w = 0
+b = 0
+lr = 0.1
+epochs = 1000
+
+def sigmoid(z):
+    return None
+
+for epoch in range(epochs):
+
+    z = None
+    y_pred = None
+
+    loss = None
+
+    dw = None
+    db = None
+
+    w = None
+    b = None
+
+def predict(x):
+    z = None
+    prob = None
+    return None
